6a78e3e029a7504e290c9fbe4ca93c675270df98
“Sharon <sharon.paz.00@gmail.com>
2022-07-08 13:06:44 -0700

    two ways to add an image

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
 const About = () => {
@@ -6,8 +7,14 @@ const About = () => {
 			<h1>About</h1>
 			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab cum culpa mollitia corporis corrupti nisi deleniti exercitationem ad deserunt! Pariatur sed perferendis repellat nisi labore odio? Non aliquam illo quidem.</p>
 			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab cum culpa mollitia corporis corrupti nisi deleniti exercitationem ad deserunt! Pariatur sed perferendis repellat nisi labore odio? Non aliquam illo quidem.</p>
+			<picture>
+				<source srcSet="/oxle-icon.gif" />
+				<img src="/oxle-icon.gif" alt="Oxleberry logo - three little monsters" />
+			</picture>
+			<br />
+			<Image src="/oxle-icon.gif" width={291} height={100} alt="Oxleberry logo - three little monsters"/>
 		</div>
-		);
+	);
 }
 
 export default About;
