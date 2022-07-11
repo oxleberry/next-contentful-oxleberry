db8cc859d57d1c7fecb9f308781ce1a4d670eba6
“Sharon <sharon.paz.00@gmail.com>
2022-07-11 08:11:25 -0700

    add desktop navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,35 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
+import Image from 'next/image'
+import styles from '/styles/components/_Navbar.module.scss'
 
 const Navbar = () => {
+	const router = useRouter();
+
 	return (
 		<div>
-			<nav>
-				<Link href="/"><a>Oxleberry</a></Link>
-				<Link href="/"><a>Portfolio</a></Link>
-				<Link href="/creative-coding"><a>Creative Coding</a></Link>
-				<Link href="/about"><a>About</a></Link>
-			</nav>
+			{/* Desktop */}
+			<div className={styles["desktop-nav-full-width-wrapper"]}>
+				<nav className={styles["desktop-nav"]} aria-label="Primary Naviation" role="navigation">
+					<ul className={styles["left-nav"]}>
+						<li><Link href="/"><a>Oxleberry</a></Link></li>
+						<li><Link href="/"><a className={router.pathname == "/" ? `${styles.active}` : ""}>Portfolio</a></Link></li>
+						<li><Link href="/creative-coding"><a className={router.pathname == "/creative-coding" ? `${styles.active}` : ""}>Creative Coding</a></Link></li>
+						<li><Link href="/about"><a className={router.pathname == "/about" ? `${styles.active}` : ""}>About</a></Link></li>
+					</ul>
+					<ul className={styles["right-nav"]}>
+						<li>
+							<Link href='/'>
+								<picture>
+									<source srcSet="/oxle-icon.gif" />
+									<img src="/oxle-icon.gif" alt="Oxleberry logo - three little monsters" />
+								</picture>
+								{/* <Image src="/oxle-icon.gif" width={134} height={46} alt="Oxleberry logo - three little monsters"/> */}
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</div>
 	);
 }
