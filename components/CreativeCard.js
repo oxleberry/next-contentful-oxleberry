d394288dcdd40edcf045f4dc95d82e0ab107aac1
“Sharon <sharon.paz.00@gmail.com>
2022-07-14 08:41:12 -0700

    display Creative Card with content from CMS

--- components/CreativeCard.js
+++ components/CreativeCard.js
@@ -2,9 +2,11 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 const CreativeCard = ({ card }) => {
+	const { title, order, url, slug, image } = card.fields;
+
 	return (
-		<article className="card-item bounce">
-			<Link href='/creative-coding/all-stars'>
+		<article id={order} className={`card-item bounce ${slug}`}>
+			<Link href={url ? `${url}` : `/creative-coding/${slug}`}>
 				<a>
 					<div className="img-block">
 						<Image src="/creative-card/all-stars.png" width={300} height={324} alt="Oxle Miitomo"/>
@@ -14,7 +16,7 @@ const CreativeCard = ({ card }) => {
 						</picture> */}
 					</div>
 					<div className="text-block">
-						<h2>All Star Randomizer</h2>
+						<h2 dangerouslySetInnerHTML={{__html: title}} />
 					</div>
 				</a>
 			</Link>
--- pages/creative-coding/index.js
+++ pages/creative-coding/index.js
@@ -2,16 +2,16 @@ import Head from 'next/head'
 import { createClient } from 'contentful'
 
 import Header from '../../components/Header'
-import CreativeCard from '../../components/CreativeCard';
+import CreativeCard from '../../components/CreativeCard'
 
 
 export async function getStaticProps() {
 	const client = createClient({
 		space: process.env.CONTENTFUL_SPACE_ID,
 		accessToken: process.env.CONTENTFUL_ACCESS_KEY
-	})
+	});
 
-	const res = await client.getEntries({ content_type: 'creativeCard' })
+	const res = await client.getEntries({ content_type: 'creativeCard' });
 
 	return {
 		props: {
@@ -22,10 +22,11 @@ export async function getStaticProps() {
 
 export default function CreativeCoding({ creativeCards }) {
 
-	console.log(creativeCards)
+	// console.log(creativeCards)
+	creativeCards.sort((a, b) => a.fields.order - b.fields.order);
 
 	return (
-		<div>
+		<>
 			<Head>
 				<title>Oxleberry | Creative Coding</title>
 				<meta name="description" content="Oxleberry Creative Coding - a collection of coding projects, each built to learn and explore front-end development concepts using CSS and Javascript." />
@@ -33,16 +34,11 @@ export default function CreativeCoding({ creativeCards }) {
 			<main className="page-backboard">
 				<Header headline="Creative Coding Projects"></Header>
 				<section className="creative-cards">
-					<CreativeCard />
-					<CreativeCard />
-					<CreativeCard />
-					<CreativeCard />
-					<CreativeCard />
-					<CreativeCard />
-					<CreativeCard />
-					<CreativeCard />
+					{creativeCards.map((card) => {
+						return <CreativeCard key={card.sys.id} card={card}/>
+					})}
 				</section>
 			</main>
-		</div>
+		</>
 	);
 }
