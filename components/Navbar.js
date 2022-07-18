464bb0d584fe73f20112ee353723a6da7b8fbc06
“Sharon <sharon.paz.00@gmail.com>
2022-07-18 09:08:25 -0700

    update navbar to highlight parent links on sub-pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,36 +1,40 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router';
-import Image from 'next/image'
-import styles from '/styles/components/_Navbar.module.scss'
 
 const Navbar = () => {
 	const router = useRouter();
 
+	// Keep Link active if in a sub-page
+	function checkPath(path, subDirectory) {
+		if (path.startsWith(subDirectory)) {
+			return true;
+		}
+	}
+
 	return (
-		<div>
+		<>
 			{/* Desktop */}
-			<div className={styles["desktop-nav-full-width-wrapper"]}>
-				<nav className={styles["desktop-nav"]} aria-label="Primary Naviation" role="navigation">
-					<ul className={styles["left-nav"]}>
+			<nav className="desktop-nav-full-width-wrapper" aria-label="Primary Naviation" role="navigation">
+				<div className="desktop-nav">
+					<ul className="left-nav">
 						<li><Link href="/"><a>Oxleberry</a></Link></li>
-						<li><Link href="/"><a className={router.pathname == "/" ? `${styles.active}` : ""}>Portfolio</a></Link></li>
-						<li><Link href="/creative-coding"><a className={router.pathname == "/creative-coding" ? `${styles.active}` : ""}>Creative Coding</a></Link></li>
-						<li><Link href="/about"><a className={router.pathname == "/about" ? `${styles.active}` : ""}>About</a></Link></li>
+						<li><Link href="/"><a className={router.pathname == "/" ? "active" : ""}>Portfolio</a></Link></li>
+						<li><Link href="/creative-coding"><a className={checkPath(router.pathname, '/creative-coding') ? "active" : ""}>Creative Coding</a></Link></li>
+						<li><Link href="/about"><a className={checkPath(router.pathname, '/about') ? "active" : ""}>About</a></Link></li>
 					</ul>
-					<ul className={styles["right-nav"]}>
+					<ul className="right-nav">
 						<li>
 							<Link href='/'>
 								<picture>
 									<source srcSet="/oxle-icon.gif" />
 									<img src="/oxle-icon.gif" alt="Oxleberry logo - three little monsters" />
 								</picture>
-								{/* <Image src="/oxle-icon.gif" width={134} height={46} alt="Oxleberry logo - three little monsters"/> */}
 							</Link>
 						</li>
 					</ul>
-				</nav>
-			</div>
-		</div>
+				</div>
+			</nav>
+		</>
 	);
 }
 
