e857c8e1ecc4320c54a5f1d7c8368550c995553b
“Sharon <sharon.paz.00@gmail.com>
2022-07-20 12:00:41 -0700

    add about page tiles

---
+++ components/AboutTile.js
@@ -0,0 +1,62 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+
+function AboutTile({ tile }) {
+	const { title, order, type, image, description, url, slug, isSprite } = tile.fields;
+
+	return (
+		// with Sprite
+		isSprite ?
+			<div id={order} className={`card-block sprite-container ${title}`}>
+				<Link href={url ? `${url}` : `/about/${slug}`}>
+					<a>
+						<div className="img-block sprite" style={{background: `url(https:${image.fields.file.url})`}}></div>
+					</a>
+				</Link>
+			</div>
+		:
+
+		// with Links
+		url || slug ?
+		<div id={order} className={`card-block ${title}`}>
+			<Link href={url ? `${url}` : `/about/${slug}`}>
+				<a>
+				{image ?
+					<div className={`img-block${type === 'Image & Text Tile' ? ` flat-bottom-edge` : ''}`}>
+						<Image
+							src={`https:${image.fields.file.url}`}
+							width={image.fields.file.details.image.width}
+							height={image.fields.file.details.image.height}
+							alt={image.fields.description} />
+						</div>
+					: null }
+					{description ?
+						<div className="text-block">{documentToReactComponents(description)}</div>
+					: null }
+				</a>
+			</Link>
+		</div>
+		:
+
+		// without Links
+		<div id={order} className={`card-block ${title}`}>
+			{image ?
+				<div className={`img-block${type === 'Image & Text Tile' ? ` flat-bottom-edge` : ''}`}>
+					<Image
+						src={`https:${image.fields.file.url}`}
+						width={image.fields.file.details.image.width}
+						height={image.fields.file.details.image.height}
+						alt={image.fields.description} />
+				</div>
+			: null }
+			{description ?
+				<div className="text-block">{documentToReactComponents(description)}</div>
+			: null }
+		</div>
+	);
+}
+
+export default AboutTile;
---
+++ pages/about/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import { createClient } from 'contentful'
+
+import Header from '../../components/Header'
+import AboutTile from '../../components/AboutTile'
+
+
+export async function getStaticProps() {
+	const client = createClient({
+		space: process.env.CONTENTFUL_SPACE_ID,
+		accessToken: process.env.CONTENTFUL_ACCESS_KEY
+	});
+
+	const res = await client.getEntries({ content_type: 'aboutTile' });
+
+	return {
+		props: {
+			aboutTiles: res.items
+		}
+	}
+}
+
+export default function About({ aboutTiles }) {
+	// console.log(aboutTiles)
+	aboutTiles.sort((a, b) => a.fields.order - b.fields.order);
+
+	return (
+		<>
+			<Head>
+				<title>Oxleberry | About</title>
+				<meta name="description" content="Oxleberry About - a collage of interests and activities from the life of Sharon Paz." />
+			</Head>
+			<main className="page-backboard">
+				<Header headline="Sharon Paz"></Header>
+				<section className="about-tiles">
+					{aboutTiles.map((tile) => {
+						return <AboutTile key={tile.sys.id} tile={tile}/>
+					})}
+				</section>
+			</main>
+		</>
+	);
+}
