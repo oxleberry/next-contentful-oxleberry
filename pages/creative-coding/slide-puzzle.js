a505507415e2a1a200bbd671200647abb0f57a0d
“Sharon <sharon.paz.00@gmail.com>
2023-03-22 09:48:32 -0700

    feat(slide puzzle): add reference image

--- pages/creative-coding/slide-puzzle.js
+++ pages/creative-coding/slide-puzzle.js
@@ -10,7 +10,15 @@ const SlidePuzzle = () => {
 				<meta name="description" content="Oxleberry Slide Puzzle - A customizable slide puzzle game. Slide the tiles to match the original image." />
 			</Head>
 			<main className="full-backboard slide-puzzle-page">
-				<Header headline="Slide Puzzle" alt={true}></Header>
+				<section className="intro">
+					<Header headline="Slide Puzzle" alt={true}></Header>
+					<p className="description">Slide the tiles to restore the original image. Customize the puzzle by updating the setting levels and uploading your own image!</p>
+					<p className="description">This game has been adapted from The Coding Train - <a href="https://www.youtube.com/watch?v=uQZLzhrzEs4">Coding Challenge 165</a> tutorial video.</p>
+				</section>
+				<section className="slide-puzzle-container">
+					<div className="reference-image"></div>
+					<div className="puzzle-board"></div>
+				</section>
 			</main>
 		</>
 	);
