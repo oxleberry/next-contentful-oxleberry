ef32bbe7fdaf0864a307ca4bd7be8edfbc008b17
“Sharon <sharon.paz.00@gmail.com>
2023-06-14 09:38:46 -0700

    feat(nth-child): create a grid item component

--- pages/creative-coding/nth-child.js
+++ pages/creative-coding/nth-child.js
@@ -2,6 +2,13 @@ import Head from 'next/head'
 import Header from '../../components/Header'
 
 
+const GridItem = (props) => {
+	return (
+		<div className={`grid-item ${props.column} ${props.item}`}></div>
+	);
+}
+
+
 export default function nthChild() {
 
 	return (
@@ -12,7 +19,10 @@ export default function nthChild() {
 			</Head>
 			<main className="full-backboard nth-child-page">
 				<Header headline="nth-child Examples" alt={true}></Header>
-				<section className="">
+				<section className="example example-01">
+					<div className="grid-container">
+						<GridItem column='column-8' item='item-1'/>
+				</div>
 				</section>
 			</main>
 		</>
