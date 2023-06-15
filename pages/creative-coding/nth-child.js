f7be5b752bc90ca4f84edc10d6c1489e21433e3c
“Sharon <sharon.paz.00@gmail.com>
2023-06-15 08:37:02 -0700

    feat(nth-child): create a grid column component

--- pages/creative-coding/nth-child.js
+++ pages/creative-coding/nth-child.js
@@ -2,9 +2,23 @@ import Head from 'next/head'
 import Header from '../../components/Header'
 
 
+// const items = [0,1,2,3,4,5,6,7,8,9];
+const itemsPerColumn = Array.from({ length: 10 }, (value, index) => index);
+
+
 const GridItem = (props) => {
 	return (
 		<div className={`grid-item ${props.column} ${props.item}`}></div>
+	);
+}
+
+const GridColumn = (props) => {
+	return (
+		<>
+			{itemsPerColumn.map((item, idx) => (
+				<GridItem key={idx} column={props.column} item={`item-${idx + 1}`} />
+			))}
+		</>
 	);
 }
 
@@ -21,7 +35,10 @@ export default function nthChild() {
 				<Header headline="nth-child Examples" alt={true}></Header>
 				<section className="example example-01">
 					<div className="grid-container">
-						<GridItem column='column-8' item='item-1'/>
+						<GridColumn column="column-1"/>
+						<GridColumn column="column-2"/>
+						<GridColumn column="column-3"/>
+						{/* <GridItem column='column-8' item='item-1'/> */}
 				</div>
 				</section>
 			</main>
