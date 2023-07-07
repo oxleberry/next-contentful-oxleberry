eb75a3cc64cd0b13c6a08e8de79622967ca5858f
Sharon <sharon.paz.00@gmail.com>
2023-07-07 08:59:04 -0700

    feat(stickers): grab images from CMS

--- pages/about/animated-stickers.js
+++ pages/about/animated-stickers.js
@@ -1,8 +1,27 @@
 import Head from 'next/head'
+import { createClient } from 'contentful'
 import Header from '../../components/Header'
 
 
-export default function animatedSticker() {
+// get CMS content =================
+const client = createClient({
+	space: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_ACCESS_KEY
+});
+
+export async function getStaticProps() {
+	const res = await client.getEntries({ content_type: 'animatedStickers' });
+	return {
+		props: {
+			animatedStickers: res.items
+		}
+	}
+}
+
+
+export default function animatedSticker({ animatedStickers }) {
+	const { title, stickerImages } = animatedStickers[0].fields;
+
 	return (
 		<>
 			<Head>
@@ -10,40 +29,20 @@ export default function animatedSticker() {
 				<meta name="description" content="Oxleberry Stickers - A short run of iMessage animated stickers that was available for iPhones from 2020-2021." />
 			</Head>
 			<main className="page-backboard animated-stickers-page">
-				<Header headline="Oxleberry Stickers" alt={true}></Header>
+				<Header headline={title} alt={true}></Header>
 				<div className="grid">
-					<picture>
-						<source srcSet="/animated-stickers/blue-monster-chomp-animation.gif" />
-						<img className="sticker-image" src="/animated-stickers/blue-monster-chomp-animation.gif" alt="animated sticker" />
-					</picture>
-					<picture>
-						<source srcSet="/animated-stickers/tree-monster-animation.gif" />
-						<img className="sticker-image" src="/animated-stickers/tree-monster-animation.gif" alt="animated sticker" />
-					</picture>
-					<picture>
-						<source srcSet="/animated-stickers/orange-monster-animation.gif" />
-						<img className="sticker-image" src="/animated-stickers/orange-monster-animation.gif" alt="animated sticker" />
-					</picture>
-					<picture>
-						<source srcSet="/animated-stickers/star-balloon-animation.gif" />
-						<img className="sticker-image" src="/animated-stickers/star-balloon-animation.gif" alt="animated sticker" />
-					</picture>
-					<picture>
-						<source srcSet="/animated-stickers/blue-monster-chomp-animation.gif" />
-						<img className="sticker-image" src="/animated-stickers/blue-monster-chomp-animation.gif" alt="animated sticker" />
-					</picture>
-					<picture>
-						<source srcSet="/animated-stickers/tree-monster-animation.gif" />
-						<img className="sticker-image" src="/animated-stickers/tree-monster-animation.gif" alt="animated sticker" />
-					</picture>
-					<picture>
-						<source srcSet="/animated-stickers/orange-monster-animation.gif" />
-						<img className="sticker-image" src="/animated-stickers/orange-monster-animation.gif" alt="animated sticker" />
-					</picture>
-					<picture>
-						<source srcSet="/animated-stickers/star-balloon-animation.gif" />
-						<img className="sticker-image" src="/animated-stickers/star-balloon-animation.gif" alt="animated sticker" />
-					</picture>
+					{stickerImages.map((image, idx) => {
+						return (
+							<picture key={idx} className={`img-${idx + 1}`}>
+								<source srcSet={image.fields.file.url} />
+								<img
+									className="sticker-image"
+									src={image.fields.file.url}
+									alt={image.fields.description}
+								/>
+							</picture>
+						)
+					})}
 				</div>
 			</main>
 		</>
