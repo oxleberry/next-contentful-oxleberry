c6b13eec840be3c33607ec8ffa214e8b4974a31d
Sharon <sharon.paz.00@gmail.com>
2023-07-12 09:33:20 -0700

    refactor(stickers): CMS data for each sticker image

--- pages/about/animated-stickers.js
+++ pages/about/animated-stickers.js
@@ -20,7 +20,9 @@ export async function getStaticProps() {
 
 
 export default function animatedSticker({ animatedStickers }) {
-	const { title, stickerImages, appDescription, appImages } = animatedStickers[0].fields;
+	// const { title, stickerImages, appDescription, appImages } = animatedStickers[0].fields;
+	animatedStickers.sort((a, b) => a.fields.order - b.fields.order);
+	console.log(animatedStickers);
 
 	return (
 		<>
@@ -29,17 +31,17 @@ export default function animatedSticker({ animatedStickers }) {
 				<meta name="description" content="Oxleberry Stickers - A short run of iMessage animated stickers that was available for iPhones from 2020-2021." />
 			</Head>
 			<main className="page-backboard animated-stickers-page">
-				<Header headline={title} alt={true}></Header>
+				<Header headline="Oxleberry Stickers" alt={true}></Header>
 				{/* Sitckers Grid */}
 				<div className="grid">
-					{stickerImages.map((image, idx) => {
+					{animatedStickers.map((image, idx) => {
 						return (
 							<picture key={idx} className={`img-${idx + 1}`}>
-								<source srcSet={image.fields.file.url} />
+								<source srcSet={image.fields.image.fields.file.url} />
 								<img
 									className="sticker-image"
-									src={image.fields.file.url}
-									alt={image.fields.description}
+									src={image.fields.image.fields.file.url}
+									alt={image.fields.image.fields.description}
 								/>
 							</picture>
 						)
@@ -47,8 +49,8 @@ export default function animatedSticker({ animatedStickers }) {
 				</div>
 
 				<hr />
-				<p className="description">{appDescription}</p>
-				{appImages.map((image, idx) => {
+				<p className="description">A short run of iMessage animated stickers that was available for iPhones from 2020-2021.</p>
+				{/* {appImages.map((image, idx) => {
 					return (
 						<picture key={idx} className={`img-${idx + 1}`}>
 							<source srcSet={image.fields.file.url} />
@@ -59,7 +61,7 @@ export default function animatedSticker({ animatedStickers }) {
 							/>
 						</picture>
 					)
-				})}
+				})} */}
 			</main>
 		</>
 	);
