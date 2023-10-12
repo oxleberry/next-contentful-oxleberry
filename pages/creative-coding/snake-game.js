c95ead68c3a627d53eb50ca272f2ddd6203b77b5
Sharon <sharon.paz.00@gmail.com>
2023-10-12 14:25:55 -0700

    feat(snake-game): draw game board and apple

--- pages/creative-coding/snake-game.js
+++ pages/creative-coding/snake-game.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Head from 'next/head'
 import Header from '../../components/Header'
 
@@ -10,39 +11,68 @@ const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
 })
 
 
-export default function SnakeGame() {
-	const y = 50;
-	let direction = '^';
+class SnakeGame extends React.Component {
+	constructor() {
+		super();
 
-	const setup = (p5, canvasParentRef) => {
-		p5.createCanvas(500, 400).parent(canvasParentRef)
-	}
-
-	const draw = p5 => {
-		p5.background(0);
-		p5.ellipse(p5.width / 2, y, 70, 70);
-		if (y > p5.height) direction = '';
-		if (y < 0) {
-			direction = '^';
-		}
-		if (direction === '^') y += 2;
-		else y -= 1;
+		// Variables =================
+		this.unitSize = 22;
+		this.canvasSize = 17 * this.unitSize; // number needs to be divisible by the unit size
+		this.foodPos = {
+			x: this.unitSize * 8,
+			y: this.unitSize * 8
+		};
 	}
 
 
-	return (
-		<>
-			<Head>
-				<title>Oxleberry | Snake Game</title>
-				<meta name="description" content="Oxleberry Snake Game - the classic Snake Game using p5.js" />
-			</Head>
-			<main className="full-backboard snake-game-page">
-				<Header headline="Snake Game" isSubPage={true}></Header>
-				<p>Eat the red apple, but don&apos;t hit the sides. <br />Use keyboard arrows to move the snake.</p>
+	// Game Functions =================
+	drawGameBoard(p5) {
+		p5.background(220); // lt grey
+		p5.stroke(144, 186, 204); // blue
+		p5.strokeWeight(4);
+		p5.fill(30); // soft black
+		p5.rect((this.unitSize - 2), (this.unitSize - 2), (p5.width - this.unitSize * 2) + 4, (p5.height - this.unitSize * 2) + 4);
+	}
 
-				{/* SNAKE GAME */}
-				<Sketch setup={setup} draw={draw} />
-			</main>
-		</>
-	);
+	drawFood(p5) {
+		p5.strokeWeight(2);
+		p5.stroke(163, 56, 37);
+		p5.fill(209, 82, 60);
+		p5.rect (this.foodPos.x, this.foodPos.y, this.unitSize, this.unitSize);
+	}
+
+
+	// p5 Drawing Library functions =================
+	setup = (p5, canvasParentRef) => {
+		p5.createCanvas(this.canvasSize, this.canvasSize).parent(canvasParentRef);
+		p5.frameRate(8);
+		this.drawGameBoard(p5);
+		this.drawFood(p5);
+	};
+
+	draw = p5 => {
+		this.drawGameBoard(p5);
+		this.drawFood(p5);
+	};
+
+
+	render() {
+		return (
+			<>
+				<Head>
+					<title>Oxleberry | Snake Game</title>
+					<meta name="description" content="Oxleberry Snake Game - The classic Snake Game using p5.js" />
+				</Head>
+				<main className="full-backboard snake-game-page">
+					<Header headline="Snake Game" isSubPage={true}></Header>
+					<p>Eat the red apple, but don&apos;t hit the sides. <br />Use keyboard arrows to move the snake.</p>
+
+					{/* SNAKE GAME */}
+					<Sketch setup={this.setup} draw={this.draw} />
+				</main>
+			</>
+		);
+	}
 }
+
+export default SnakeGame;
