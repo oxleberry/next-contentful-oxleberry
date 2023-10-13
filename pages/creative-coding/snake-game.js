31ac7392cf54ba137d0b0ae1957ffe3b13bae31b
Sharon <sharon.paz.00@gmail.com>
2023-10-13 10:38:14 -0700

    feat(snake-game): draw and animate snake

--- pages/creative-coding/snake-game.js
+++ pages/creative-coding/snake-game.js
@@ -14,10 +14,21 @@ const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
 class SnakeGame extends React.Component {
 	constructor() {
 		super();
+		this.state = {
+			direction: {
+				x: 1, // positive (1) moves right, negative (-1) moves left, (0) does not move on x-axis
+				y: 0, // positive (1) moves down, negative (-1) moves up, (0) does not move on y-axis
+			}
+		};
+
 
 		// Variables =================
 		this.unitSize = 22;
 		this.canvasSize = 17 * this.unitSize; // number needs to be divisible by the unit size
+		this.snakePos = {
+			x: this.unitSize,
+			y: this.unitSize,
+		};
 		this.foodPos = {
 			x: this.unitSize * 8,
 			y: this.unitSize * 8
@@ -34,11 +45,30 @@ class SnakeGame extends React.Component {
 		p5.rect((this.unitSize - 2), (this.unitSize - 2), (p5.width - this.unitSize * 2) + 4, (p5.height - this.unitSize * 2) + 4);
 	}
 
+	drawSnake(p5) {
+		// draws the snake head
+		p5.stroke(152, 168, 102);
+		p5.fill(211, 229, 165);
+		p5.rect(this.snakePos.x, this.snakePos.y, this.unitSize, this.unitSize);
+	}
+
 	drawFood(p5) {
 		p5.strokeWeight(2);
 		p5.stroke(163, 56, 37);
 		p5.fill(209, 82, 60);
 		p5.rect (this.foodPos.x, this.foodPos.y, this.unitSize, this.unitSize);
+	}
+
+	// update snake head position
+	// returns object = {x: 20, y: 20};
+	moveSnakeHead(p5) {
+		// movement of head by 1 unit to the next position
+		let x = this.snakePos.x + this.state.direction.x * this.unitSize;
+		let y = this.snakePos.y + this.state.direction.y * this.unitSize;
+		// constrains the position so the snake cannot go out of the game board
+		x = p5.constrain(x, 0 + this.unitSize, p5.width - this.unitSize * 2);
+		y = p5.constrain(y, 0 + this.unitSize, p5.height - this.unitSize * 2);
+		return {x, y};
 	}
 
 
@@ -47,12 +77,15 @@ class SnakeGame extends React.Component {
 		p5.createCanvas(this.canvasSize, this.canvasSize).parent(canvasParentRef);
 		p5.frameRate(8);
 		this.drawGameBoard(p5);
+		this.drawSnake(p5);
 		this.drawFood(p5);
 	};
 
 	draw = p5 => {
 		this.drawGameBoard(p5);
 		this.drawFood(p5);
+		this.snakePos = this.moveSnakeHead(p5);
+		this.drawSnake(p5);
 	};
 
 
