1214ab8cc80aad8bacacc44bc238ff9ee6af34d7
Sharon <sharon.paz.00@gmail.com>
2024-06-14 15:11:40 -0700

    feat(share-card): setup page

---
+++ pages/creative-coding/share-card.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Header from '../../components/Header'
+
+export default function ShareCard() {
+	return (
+		<>
+			<Head>
+				<title>Oxleberry | Share Card</title>
+				<meta name="description" content="Oxleberry Share Card - Send a custom greeting" />
+			</Head>
+			<main className="full-backboard share-card-page">
+				<Header headline="Share Card" isSubPage={true}></Header>
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab cum culpa mollitia corporis corrupti nisi deleniti exercitationem ad deserunt! Pariatur sed perferendis repellat nisi labore odio? Non aliquam illo quidem.</p>
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab cum culpa mollitia corporis corrupti nisi deleniti exercitationem ad deserunt! Pariatur sed perferendis repellat nisi labore odio? Non aliquam illo quidem.</p>
+			</main>
+		</>
+	);
+}
