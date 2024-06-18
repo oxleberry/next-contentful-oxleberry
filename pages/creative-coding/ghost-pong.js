78f25dbf3c541317ad61cd24d90c717d5e926230
Sharon <sharon.paz.00@gmail.com>
2024-06-18 10:38:24 -0700

    feat(ghost-pong): move ghost

--- pages/creative-coding/ghost-pong.js
+++ pages/creative-coding/ghost-pong.js
@@ -23,6 +23,8 @@ class GhostPong extends React.Component {
 		this.ghostSize = 50;
 		this.ghostXPos = this.canvasSizeWidth / 2;
 		this.ghostYPos = this.canvasSizeHeight / 2;
+		this.xSpeed = 2;
+		this.ySpeed = 2;
 	}
 
 		drawGameBoardBg(p5) {
@@ -45,6 +47,10 @@ class GhostPong extends React.Component {
 			}
 		}
 
+		moveGhost() {
+			this.ghostXPos = this.ghostXPos + this.xSpeed;
+			this.ghostYPos = this.ghostYPos + this.ySpeed;
+		}
 
 		// p5 Drawing Library functions =================
 		setup = (p5, canvasParentRef) => {
@@ -71,6 +77,7 @@ class GhostPong extends React.Component {
 
 		draw = p5 => {
 			this.drawGameBoardBg(p5);
+			this.moveGhost();
 			this.drawGhost(p5);
 			this.drawGameBoardBorder(p5);
 		}
