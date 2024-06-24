a2edf4fdb1edd0e865489421952352d8486fe334
Sharon <sharon.paz.00@gmail.com>
2024-06-24 14:38:52 -0700

    feat(share-card): add canvas

--- pages/creative-coding/share-card.js
+++ pages/creative-coding/share-card.js
@@ -9,6 +9,8 @@ export default function ShareCard() {
 	const [galleryImage, setGalleryImage] = useState('');
 	const [isCustomText, setIsCustomText] = useState(false);
 
+	const shareFileRef = useRef(null);
+
 	function colorInputHandler(event) {
 		let value = event.target.value;
 		setColorInput(value);
@@ -29,6 +31,18 @@ export default function ShareCard() {
 	function galleryClickHandler(event) {
 		let imagePath = event.target.src;
 		setGalleryImage(imagePath);
+	}
+
+	function shareClickHandler() {
+		// draw canvas
+		const canvas = document.createElement('canvas');
+		canvas.width = 436;
+		canvas.height = 604;
+		const context = canvas.getContext('2d');
+		context.fillStyle = 'lightblue';
+		context.fillRect(0, 0, 436, 604);
+		shareFileRef.current.appendChild(canvas);
+		return canvas;
 	}
 
 	return (
@@ -74,7 +88,7 @@ export default function ShareCard() {
 								className="custom-text"
 								name="custom-text"
 								type="text"
-								maxlength="70"
+								maxLength="70"
 								placeholder="Add Your Text Here"
 								value={textInput}
 								onChange={textInputHandler}
@@ -110,15 +124,32 @@ export default function ShareCard() {
 						</div>
 						{/* Share Card Button */}
 						<div className="option option-share">
-							<button className="share-card-button">Share Card 
-								<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 48 48">
+							<button 
+								type="button" 
+								className="share-card-button"
+								onClick={shareClickHandler}
+							>Share Card
+								<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 42 42">
 									<path d="M 35.478516 5.9804688 A 2.0002 2.0002 0 0 0 34.085938 9.4140625 L 35.179688 10.507812 C 23.476587 10.680668 14 20.256715 14 32 A 2.0002 2.0002 0 1 0 18 32 C 18 22.427546 25.627423 14.702715 35.154297 14.517578 L 34.085938 15.585938 A 2.0002 2.0002 0 1 0 36.914062 18.414062 L 41.236328 14.091797 A 2.0002 2.0002 0 0 0 41.228516 10.900391 L 36.914062 6.5859375 A 2.0002 2.0002 0 0 0 35.478516 5.9804688 z M 12.5 6 C 8.9338464 6 6 8.9338464 6 12.5 L 6 35.5 C 6 39.066154 8.9338464 42 12.5 42 L 35.5 42 C 39.066154 42 42 39.066154 42 35.5 L 42 28 A 2.0002 2.0002 0 1 0 38 28 L 38 35.5 C 38 36.903846 36.903846 38 35.5 38 L 12.5 38 C 11.096154 38 10 36.903846 10 35.5 L 10 12.5 C 10 11.096154 11.096154 10 12.5 10 L 20 10 A 2.0002 2.0002 0 1 0 20 6 L 12.5 6 z"></path>
 								</svg>
 							</button>
 						</div>
 					</section>
 
-					<section className="canvas-container"></section>
+					<section 
+						// id="canvas-container"
+						className="canvas-container"
+						ref={shareFileRef} 
+					>
+						{/* <img className="gallery-image" src="/creative-coding-pages/share-card/images/rice-cracker-flower.png" /> */}
+						{/* <img className="gallery-image" src="/creative-coding-pages/share-card/images/flower-stamp.png" /> */}
+						{/* <img
+							ref={shareFileRef} 
+							className="gallery-image" 
+							src="/creative-coding-pages/share-card/images/food-art-owls.png" /> */}
+					</section>
+
+					{/* <canvas className="canvas-share-card"></canvas> */}
 
 				</main>
 			</div>
