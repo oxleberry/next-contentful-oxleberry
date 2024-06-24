fb7e65b750854838be43b43e6803f377f721a813
Sharon <sharon.paz.00@gmail.com>
2024-06-24 16:07:41 -0700

    feat(share-card): add SVG text

--- pages/creative-coding/share-card.js
+++ pages/creative-coding/share-card.js
@@ -34,7 +34,7 @@ export default function ShareCard() {
 	}
 
 	function shareClickHandler() {
-		// draw canvas
+		// create canvas
 		const canvas = document.createElement('canvas');
 		canvas.width = 436;
 		canvas.height = 604;
@@ -42,7 +42,11 @@ export default function ShareCard() {
 		context.fillStyle = 'lightblue';
 		context.fillRect(0, 0, 436, 604);
 		shareFileRef.current.appendChild(canvas);
-		return canvas;
+		// return canvas;
+
+		// create share SVG
+
+
 	}
 
 	return (
@@ -88,7 +92,7 @@ export default function ShareCard() {
 								className="custom-text"
 								name="custom-text"
 								type="text"
-								maxLength="70"
+								maxLength="20"
 								placeholder="Add Your Text Here"
 								value={textInput}
 								onChange={textInputHandler}
@@ -99,22 +103,22 @@ export default function ShareCard() {
 						<div className="option option-image">
 							<label className="option-label">Choose a gallery image:</label>
 							<div className="gallery-container">
-								<button 
-									type="button" 
+								<button
+									type="button"
 									className="gallery-image-button button-black"
 									onClick={galleryClickHandler}
 								>
 									<img className="gallery-image" src="/creative-coding-pages/share-card/images/geo-flower.svg" />
 								</button>
-								<button 
-									type="button" 
+								<button
+									type="button"
 									className="gallery-image-button button-black"
 									onClick={galleryClickHandler}
 								>
 									<img className="gallery-image" src="/creative-coding-pages/share-card/images/sugar-skull.svg" />
 								</button>
-								<button 
-									type="button" 
+								<button
+									type="button"
 									className="gallery-image-button button-black"
 									onClick={galleryClickHandler}
 								>
@@ -124,32 +128,49 @@ export default function ShareCard() {
 						</div>
 						{/* Share Card Button */}
 						<div className="option option-share">
-							<button 
-								type="button" 
+							<button
+								type="button"
 								className="share-card-button"
 								onClick={shareClickHandler}
 							>Share Card
-								<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 42 42">
+								<svg className="share-icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 42 42">
 									<path d="M 35.478516 5.9804688 A 2.0002 2.0002 0 0 0 34.085938 9.4140625 L 35.179688 10.507812 C 23.476587 10.680668 14 20.256715 14 32 A 2.0002 2.0002 0 1 0 18 32 C 18 22.427546 25.627423 14.702715 35.154297 14.517578 L 34.085938 15.585938 A 2.0002 2.0002 0 1 0 36.914062 18.414062 L 41.236328 14.091797 A 2.0002 2.0002 0 0 0 41.228516 10.900391 L 36.914062 6.5859375 A 2.0002 2.0002 0 0 0 35.478516 5.9804688 z M 12.5 6 C 8.9338464 6 6 8.9338464 6 12.5 L 6 35.5 C 6 39.066154 8.9338464 42 12.5 42 L 35.5 42 C 39.066154 42 42 39.066154 42 35.5 L 42 28 A 2.0002 2.0002 0 1 0 38 28 L 38 35.5 C 38 36.903846 36.903846 38 35.5 38 L 12.5 38 C 11.096154 38 10 36.903846 10 35.5 L 10 12.5 C 10 11.096154 11.096154 10 12.5 10 L 20 10 A 2.0002 2.0002 0 1 0 20 6 L 12.5 6 z"></path>
 								</svg>
 							</button>
 						</div>
 					</section>
 
-					<section 
-						// id="canvas-container"
+					{/* <section
+						id="canvas-container"
 						className="canvas-container"
-						ref={shareFileRef} 
+						ref={shareFileRef}
 					>
-						{/* <img className="gallery-image" src="/creative-coding-pages/share-card/images/rice-cracker-flower.png" /> */}
-						{/* <img className="gallery-image" src="/creative-coding-pages/share-card/images/flower-stamp.png" /> */}
-						{/* <img
-							ref={shareFileRef} 
-							className="gallery-image" 
-							src="/creative-coding-pages/share-card/images/food-art-owls.png" /> */}
-					</section>
+						<img className="gallery-image" src="/creative-coding-pages/share-card/images/rice-cracker-flower.png" />
+						<img className="gallery-image" src="/creative-coding-pages/share-card/images/flower-stamp.png" />
+						<img
+							ref={shareFileRef}
+							className="gallery-image"
+							src="/creative-coding-pages/share-card/images/food-art-owls.png" />
+					</section> */}
 
-					{/* <canvas className="canvas-share-card"></canvas> */}
+					<section>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="share-svg"
+							x="0px"
+							y="0px"
+							viewBox="0 0 436 604"
+						>
+							<rect width="436" height="604"></rect>
+							<text
+								className="share-svg-text"
+								x="50%"
+								y="33%"
+								dominant-baseline="middle"
+								text-anchor="middle"
+							>{textInput}</text>
+						</svg>
+					</section>
 
 				</main>
 			</div>
