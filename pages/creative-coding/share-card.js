c249c0bb468d71010c94a0bbdd72e68bc9e3e377
Sharon <sharon.paz.00@gmail.com>
2024-11-08 11:08:52 -0800

    feat(share-card): add basic sections

--- pages/creative-coding/share-card.js
+++ pages/creative-coding/share-card.js
@@ -8,11 +8,16 @@ export default function ShareCard() {
 				<title>Oxleberry | Share Card</title>
 				<meta name="description" content="Oxleberry Share Card - Send a custom greeting card" />
 			</Head>
-			<main className="full-backboard share-card-page">
+			<div className="full-backboard share-card-page">
 				<Header headline="Share Card" isSubPage={true}></Header>
-				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab cum culpa mollitia corporis corrupti nisi deleniti exercitationem ad deserunt! Pariatur sed perferendis repellat nisi labore odio? Non aliquam illo quidem.</p>
-				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab cum culpa mollitia corporis corrupti nisi deleniti exercitationem ad deserunt! Pariatur sed perferendis repellat nisi labore odio? Non aliquam illo quidem.</p>
-			</main>
+				<main>
+
+					<section className="share-content-container"></section>
+					<section className="options-container"></section>
+					<section className="canvas-container"></section>
+
+				</main>
+			</div>
 		</>
 	);
 }
