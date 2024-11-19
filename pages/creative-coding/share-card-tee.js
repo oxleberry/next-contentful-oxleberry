e81279361d0674253b479aefd9071c12fe8023f3
Sharon <sharon.paz.00@gmail.com>
2024-11-19 09:36:26 -0800

    refactor(share-card-tee): refactor display garment style

--- pages/creative-coding/share-card-tee.js
+++ pages/creative-coding/share-card-tee.js
@@ -4,6 +4,27 @@ import { useEffect, useState, useRef } from 'react'
 
 export default function ShareCard() {
 	const borderWidth = 8;
+	const seedGarmentStyleData = [
+		{
+			id: 1,
+			name: 'Adult',
+			value: 'adult-tee',
+			isActive: true
+		},
+		{
+			id: 2,
+			name: 'Womens',
+			value: 'womens-tee',
+			isActive: false
+		},
+		{
+			id: 3,
+			name: 'Onesie',
+			value: 'onesie',
+			isActive: false
+		}
+	]
+
 	const seedFilterData = [
 		{
 			id: 1,
@@ -141,23 +162,6 @@ export default function ShareCard() {
 	function garmentColorHandler(event) {
 		let value = event.target.value;
 		setGarmentColor(value);
-	}
-
-	function displayGarmentOption(value, name, isActive) {
-		return (
-			<div className="option-garment">
-				<input
-					id={value}
-					className="custom-garment"
-					name="custom-garment"
-					type="radio"
-					value={value}
-					defaultChecked={isActive}
-					onChange={garmentStyleHandler}
-				/>
-				<label htmlFor="custom-garment" className="option-label">{name}</label>
-			</div>
-		)
 	}
 
 	function displayColorSwatch(colorId, hexValue) {
@@ -617,10 +621,22 @@ export default function ShareCard() {
 						{/* Option - Garment style */}
 						<div className="option-section option-garment-style">
 							<legend className="option-label">Garment style:</legend>
-							{displayGarmentOption('adult-tee', 'Adult', true)}
-							{displayGarmentOption('womens-tee', 'Womens', false)}
-							{displayGarmentOption('onesie', 'Onesie', false)}
+							{seedGarmentStyleData.map((garment, idx) =>
+								<div key={idx} className="option-garment">
+									<input
+										id={garment.value}
+										className="custom-garment"
+										name="custom-garment"
+										type="radio"
+										value={garment.value}
+										defaultChecked={garment.isActive}
+										onChange={garmentStyleHandler}
+									/>
+									<label htmlFor="custom-garment" className="option-label">{garment.name}</label>
+								</div>
+							)}
 						</div>
+
 						{/* Option Pick a Color */}
 						<div className="option-section option-garment-color">
 							<legend className="option-label">Garment color:</legend>
