2d4d850a34f0e1c638f37abf05535e614f6c1aad
Sharon <sharon.paz.00@gmail.com>
2024-12-31 08:47:40 -0800

    feat(screenprint designer): add background color picker

--- pages/creative-coding/screenprint-designer.js
+++ pages/creative-coding/screenprint-designer.js
@@ -1,18 +1,52 @@
 import Head from 'next/head'
 import Header from '../../components/Header'
+import { useEffect, useState, useRef } from 'react'
 
 export default function ScreenprintDesigner() {
+	// States =================
+	const [backgroundColor, setBackgroundColor] = useState('#000000');
+
+	// Functions =================
+	function backgroundColorHandler(event) {
+		let value = event.target.value;
+		setBackgroundColor(value);
+	}
+
+
 	return (
 		<>
 			<Head>
 				<title>Oxleberry | Screenprint Designer</title>
 				<meta name="description" content="Oxleberry Screenprint Designer - Mockup your design on a tee shirt" />
 			</Head>
-			<main className="full-backboard screenprint-designer-page">
+			<div className="full-backboard screenprint-designer-page">
 				<Header headline="Screenprint Designer" isSubPage={true}></Header>
-				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab cum culpa mollitia corporis corrupti nisi deleniti exercitationem ad deserunt! Pariatur sed perferendis repellat nisi labore odio? Non aliquam illo quidem.</p>
-				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab cum culpa mollitia corporis corrupti nisi deleniti exercitationem ad deserunt! Pariatur sed perferendis repellat nisi labore odio? Non aliquam illo quidem.</p>
-			</main>
+				<main>
+					<section className="share-content-section">
+						<div className="share-content-container" style={{background: `${backgroundColor}`}}>
+							<h2 className="hidden">Share Content</h2>
+						</div>
+					</section>
+
+					{/* Option Section */}
+					<section className="options-container">
+						<h2 className="hidden">Options</h2>
+						{/* Option Pick a Color */}
+						<div className="option option-color">
+							<label htmlFor="custom-color" className="option-label">Pick a background color:</label>
+							<input
+								id="custom-color"
+								className="custom-color"
+								name="custom-color"
+								type="color"
+								value={backgroundColor}
+								onChange={backgroundColorHandler}
+							/>
+						</div>
+					</section>
+
+				</main>
+			</div>
 		</>
 	);
 }
