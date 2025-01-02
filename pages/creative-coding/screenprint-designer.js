45cfa5fb30fd4d1426a820bd6abecbc83b1d007f
Sharon <sharon.paz.00@gmail.com>
2025-01-02 10:06:04 -0800

    feat(screenprint designer): add gallery image option

--- pages/creative-coding/screenprint-designer.js
+++ pages/creative-coding/screenprint-designer.js
@@ -3,8 +3,27 @@ import Header from '../../components/Header'
 import { useEffect, useState, useRef } from 'react'
 
 export default function ScreenprintDesigner() {
+	const initialGalleryImageData = [
+		{
+			id: 1,
+			name: 'sakura-flower',
+			url: '/creative-coding-pages/share-card/sakura-flower.png',
+		},
+		{
+			id: 2,
+			name: 'sugar-skull',
+			url: '/creative-coding-pages/share-card/sugar-skull.png',
+		},
+		{
+			id: 3,
+			name: 'flaming-bunny',
+			url: '/creative-coding-pages/share-card/flaming-bunny.png',
+		}
+	]
+
 	// States =================
 	const [backgroundColor, setBackgroundColor] = useState('#000000');
+	const [galleryImagePath, setGalleryImagePath] = useState('');
 
 	// Functions =================
 	function backgroundColorHandler(event) {
@@ -12,6 +31,10 @@ export default function ScreenprintDesigner() {
 		setBackgroundColor(value);
 	}
 
+	function galleryClickHandler(event) {
+		let imagePath = event.target.src;
+		setGalleryImagePath(imagePath);
+	}
 
 	return (
 		<>
@@ -22,9 +45,11 @@ export default function ScreenprintDesigner() {
 			<div className="full-backboard screenprint-designer-page">
 				<Header headline="Screenprint Designer" isSubPage={true}></Header>
 				<main>
+
 					<section className="share-content-section">
 						<div className="share-content-container" style={{background: `${backgroundColor}`}}>
 							<h2 className="hidden">Share Content</h2>
+							<div className="image-display" style={{backgroundImage: `url(${galleryImagePath})`}} />
 						</div>
 					</section>
 
@@ -42,6 +67,23 @@ export default function ScreenprintDesigner() {
 								value={backgroundColor}
 								onChange={backgroundColorHandler}
 							/>
+						</div>
+						{/* Option Galley Image */}
+						<div className="option option-image">
+							<label className="option-label">Choose an image:</label>
+							<div className="gallery-container">
+								{initialGalleryImageData.map((image, idx) =>
+									<button
+										key={idx}
+										type="button"
+										className="gallery-image-button button-black"
+										onClick={galleryClickHandler}>
+										<img 
+											className={`gallery-image gallery-image-${image.id}`}
+											src={image.url}/>
+									</button>
+								)}
+							</div>
 						</div>
 					</section>
 
