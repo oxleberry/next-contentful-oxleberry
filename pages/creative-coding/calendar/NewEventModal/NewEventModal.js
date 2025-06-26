929fee2af87f3aad1ba2e7bb94e6dafd1f158de9
Sharon <sharon.paz.00@gmail.com>
2025-06-26 10:12:52 -0700

    feat(calendar): add an input field to new event modal

--- pages/creative-coding/calendar/NewEventModal/NewEventModal.js
+++ pages/creative-coding/calendar/NewEventModal/NewEventModal.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 export const NewEventModal = ({ onClose }) => {
+	// States =================
+	const [title, setTitle] = useState('Event');
 
 	return(
 		<>
 			<div className="new-event-modal">
 				<p className="modal-headline">New Event</p>
+
+				<label htmlFor="event-title">Event name:</label>
+				<input
+					id="event-title"
+					className="input-text-field"
+					name="event-title"
+					value={title}
+					onChange={e => setTitle(e.target.value)}
+				/>
 
 				<button
 					onClick={onClose}
